ebe83e104834d487e4e2c3fbb8d92168fb0cf5bc
Robert Brice <rob.a.brice@gmail.comC>
2017-05-12 13:38:10 +0100

    cleaned up API

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,15 +5,18 @@ class UsersController < ApplicationController
     users = User.all
 
     render json: users.as_json({
+
+      except: [:created_at, :updated_at],
+
       include: {
 
         profiles: {
           only:[:first_name, :last_name, :address, :image]
-        
-
-
         },
         owner_groups: {
+
+          except: [:created_at, :updated_at, :user_id],
+
           include: {
             house: {
               only: [:address, :post_code]
