613f8d023ebe8f4ae23ef695d1d8a0bc66db587f
Suzanne <suzanne.aitchison@gmail.com>
2017-05-13 12:08:42 +0100

    edit user profile route created and tested

--- hooses_api/app/controllers/users_controller.rb
+++ hooses_api/app/controllers/users_controller.rb
@@ -25,12 +25,12 @@ class UsersController < ApplicationController
     })
   end
 
-  ## NEED TO MAKE SURE ONLY EVER HAS ONE PROFILE
+  
   def create_profile
     user = User.find(params[:id])
     user_profiles = user.profiles
 
-
+    ## ENSURES A PROFILE IS ONLY CREATE IF THEY DON'T HAVE ONE ALREADY
     if user_profiles.length == 0
 
       profile = Profile.create({
@@ -45,7 +45,23 @@ class UsersController < ApplicationController
   end
 
   def edit_profile
+    user = User.find(params[:id])
+    user_profile = user.profiles.first
 
+    user_profile.update_attribute(:first_name, params[:first_name])
+    user_profile.update_attribute(:last_name, params[:last_name])
+    user_profile.update_attribute(:address, params[:address])
+    user_profile.update_attribute(:image, params[:image])
+
+
+    render json: user.as_json({
+      except: [:created_at, :updated_at],
+      include: {
+        profiles: {
+          only:[:first_name, :last_name, :address, :image]
+        }
+      }
+    })
 
   end
 
