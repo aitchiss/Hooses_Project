c12ed399c4fe30e19416164993a7c4643852397a
Suzanne <suzanne.aitchison@gmail.com>
2017-05-14 11:10:25 +0100

    can delete a topic

--- hooses_api/app/controllers/topics_controller.rb
+++ hooses_api/app/controllers/topics_controller.rb
@@ -1,17 +1,30 @@
 class TopicsController < ApplicationController
 
+  ## A LIST OF ALL TOPICS ASSOCIATED WITH A HOUSE IS GIVEN THROUGH THE HOUSE ROUTE, THE TOPICS ROUTE GIVES A TOPIC, PLUS ALL THE MESSAGES RELATED TO IT
+
+  def show
+    topic = Topic.find(params[:id])
+
+    render json: topic.as_json({
+      include: {
+        messages: {
+          only: [:user_id, :content, :created_at]
+        }
+      }
+    })
+
+  end
+
   def create
-    
     topic = Topic.create({
       user_id: params[:user_id],
       house_id: params[:house_id],
       title: params[:title],
       ## maybe we want to mark status as 'open' on default?
       status: params[:status]
-      })
+    })
 
     render json: topic
-    
   end
 
   def update
@@ -19,6 +32,11 @@ class TopicsController < ApplicationController
     topic.update_attributes(topic_params)
     render json: topic
 
+  end
+
+  def destroy
+    topic = Topic.find(params[:id])
+    topic.destroy!
   end
 
 
