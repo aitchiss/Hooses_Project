ad950271ef00a21b2a5380ba8c9f02599aff1321
Robert Brice <rob.a.brice@gmail.comC>
2017-05-14 12:01:52 +0100

    added auth and services folders

--- hooses_client/client/build/bundle.js
+++ hooses_client/client/build/bundle.js
@@ -22928,6 +22928,10 @@ var _reactDom = __webpack_require__(82);
 
 var _reactDom2 = _interopRequireDefault(_reactDom);
 
+var _WelcomeContainer = __webpack_require__(233);
+
+var _WelcomeContainer2 = _interopRequireDefault(_WelcomeContainer);
+
 var _MainContainer = __webpack_require__(81);
 
 var _MainContainer2 = _interopRequireDefault(_MainContainer);
@@ -22943,18 +22947,19 @@ window.onload = function () {
     _react2.default.createElement(
       'div',
       null,
-      _react2.default.createElement(_reactRouterDom.Route, { path: '/', component: _MainContainer2.default })
+      _react2.default.createElement(_reactRouterDom.Route, { path: '/welcome', component: _WelcomeContainer2.default }),
+      _react2.default.createElement(_reactRouterDom.Route, { path: '/app', component: _MainContainer2.default })
     )
   ), document.getElementById('app'));
 };
 
-// <Route path = "/login" component = {LogInContainer} />
+// <Route path = "/welcome" component = {WelcomeContainer} />
 // <Route path = "/commuity" component = {CommunityContainer} />
 // <Route path = "/about" component = {AboutContainer} />
 // <Route path = "/logout" component = {LogOutContainer} />
 
-
 // <Route path = "/" component = {Footer} />
+
 
 // window.onload = function () {
 //   ReactDOM.render(
@@ -26274,6 +26279,78 @@ var valueEqual = function valueEqual(a, b) {
 };
 
 exports.default = valueEqual;
+
+/***/ }),
+/* 233 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _react = __webpack_require__(8);
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var WelcomeContainer = function (_React$Component) {
+  _inherits(WelcomeContainer, _React$Component);
+
+  function WelcomeContainer(props) {
+    _classCallCheck(this, WelcomeContainer);
+
+    var _this = _possibleConstructorReturn(this, (WelcomeContainer.__proto__ || Object.getPrototypeOf(WelcomeContainer)).call(this, props));
+
+    _this.state = {};
+    return _this;
+  }
+
+  _createClass(WelcomeContainer, [{
+    key: "render",
+    value: function render() {
+
+      return _react2.default.createElement(
+        "div",
+        { className: "main-content" },
+        _react2.default.createElement(
+          "div",
+          { className: "container-responsive" },
+          _react2.default.createElement(
+            "div",
+            { className: "row" },
+            _react2.default.createElement(
+              "div",
+              { className: "col-md-2" },
+              _react2.default.createElement(
+                "h1",
+                null,
+                "Welcome"
+              )
+            ),
+            _react2.default.createElement("div", { className: "col-md-8" }),
+            _react2.default.createElement("div", { className: "col-md-2" })
+          )
+        )
+      );
+    }
+  }]);
+
+  return WelcomeContainer;
+}(_react2.default.Component);
+
+exports.default = WelcomeContainer;
 
 /***/ })
 /******/ ]);
---
+++ hooses_client/client/src/services/AjaxRequest.js
@@ -0,0 +1,63 @@
+class AjaxRequest {
+
+  get(url, done){
+    const xhr = new XMLHttpRequest()
+    xhr.withCredentials = true
+    xhr.open("GET", url)
+
+    xhr.onload = () => {
+      done(null, JSON.parse(xhr.response), xhr.status)
+    }
+
+    xhr.onerror = () => {
+      done(xhr.response)
+    }
+
+    xhr.send()
+
+  }
+
+
+  post(url, payload, done){
+    const xhr = new XMLHttpRequest()
+    xhr.open("POST", url)
+    xhr.setRequestHeader("Content-Type", "application/json")
+    xhr.withCredentials = true
+
+    xhr.onload = () => {
+      done(null, JSON.parse(xhr.response))
+    }
+
+    xhr.onerror = () => {
+      done(xhr.response)
+    }
+
+    xhr.send(payload)
+
+
+  }
+
+
+  delete(url, done){
+    const xhr = new XMLHttpRequest()
+    xhr.open("DELETE", url)
+    xhr.setRequestHeader("Content-Type", "application/json")
+    xhr.withCredentials = true
+
+    xhr.onload = () => {
+      done(null, xhr.status)
+    } 
+
+    xhr.onerror = () => {
+      done(xhr.response)
+    }
+
+    xhr.send()
+
+
+  }
+
+
+}
+
+export default AjaxRequest
