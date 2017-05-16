d7902102e1a9327164b479dec86f3a1023613721
Suzanne <suzanne.aitchison@gmail.com>
2017-05-16 09:52:50 +0100

    kitchen table route provides users profile with each message

--- hooses_api/app/controllers/kitchen_table_posts_controller.rb
+++ hooses_api/app/controllers/kitchen_table_posts_controller.rb
@@ -4,6 +4,17 @@ class KitchenTablePostsController < ApplicationController
 
     posts = KitchenTablePost.where({:house_id => params[:id]})
 
-    render json: posts
+    render json: posts.as_json({
+      include: {
+        user: {
+          except: [:created_at, :updated_at],
+          include: {
+            profiles: {
+              except: [:created_at, :updated_at, :user_id, :id]
+            }
+          }
+        }
+      }
+      })
   end
 end
