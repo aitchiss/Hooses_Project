bc4a58260f93bd878888de76a527fcb1931b71e3
Robert Brice <rob.a.brice@gmail.comC>
2017-05-17 14:11:14 +0100

    added delete topic

--- hooses_client/client/src/components/Topic.jsx
+++ hooses_client/client/src/components/Topic.jsx
@@ -10,20 +10,23 @@ selectTopic(event){
   this.props.setTopicThread(this.props.id)
 }
 
+deleteTopic(){
+  this.props.deleteTopic(this.props.id)
+}
+
 render(){
 
-
-
   return (
-    <div className="media correct-pointer" onClick={this.selectTopic.bind(this)}>
+    <div className="media correct-pointer" >
 
         <div className="media-left media-top">
               <img className="media-object" src=".." alt=".."/>
         </div>
 
         <div className="media-body">
-            <h4 className="media-heading">{this.props.title}</h4>
-            <p className="media-status">Status : {this.props.status}</p>
+            <h4 className="media-heading" onClick={this.selectTopic.bind(this)}>{this.props.title}</h4>
+            <p className="media-status">Status : {this.props.status}<i className="material-icons right" role="button" onClick={this.deleteTopic.bind(this)}>delete</i></p>
+
         </div>
 
     </div>
